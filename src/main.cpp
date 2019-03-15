c6096fd99252cf6cfd137f8d4882dd0163b8c95b
manishsmandlik@gmail.com <manishsmandlik@gmail.com>
2019-03-14 22:09:48 -0700

    adding project code

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,9 @@ int main() {
   // MPC is initialized here!
   MPC mpc;
 
-  h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  double dt = 0.0;
+
+  h.onMessage([&mpc, &dt](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -40,6 +42,8 @@ int main() {
         auto j = json::parse(s);
         string event = j[0].get<string>();
         if (event == "telemetry") {
+          auto t_start = std::chrono::system_clock::now();
+
           // j[1] is the data JSON object
           vector<double> ptsx = j[1]["ptsx"];
           vector<double> ptsy = j[1]["ptsy"];
@@ -55,10 +59,46 @@ int main() {
           double steer_value;
           double throttle_value;
 
+          // predict the next state using global kinematic model
+          //double delta = j[1]["steering_angle"];
+          //double a = j[1]["throttle"];
+          //
+          //px = px + v * cos(psi) * dt;
+          //py = py + v * sin(psi) * dt;
+          //psi = psi + (v / Lf) * delta * dt;
+          //v = v + a * dt;
+
+          // Transform simulator points to vehicle coordinate system
+          Eigen::VectorXd xvals(ptsx.size());
+          Eigen::VectorXd yvals(ptsy.size());
+
+          for (unsigned int i = 0; i < ptsx.size(); i++) {
+            double dx = ptsx[i] - px;
+            double dy = ptsy[i] - py;
+
+            xvals[i] =  dx * cos(psi) + dy * sin(psi);
+            yvals[i] = -dx * sin(psi) + dy * cos(psi);
+          }
+
+          // Fit a 3rd order polynomial to vehicle points
+          auto coeffs = polyfit(xvals, yvals, 3);
+
+          // Calculate cte and epsi
+          double cte = polyeval(coeffs, 0) - 0;
+          double epsi = 0 - atan(coeffs[1]);
+
+          // Calculate steering angle and throttle using MPC
+          Eigen::VectorXd state(6);
+          //state << px, py, psi, v, cte, epsi;
+          state << 0, 0, 0, v, cte, epsi;
+          auto vars = mpc.Solve(state, coeffs);
+          steer_value = vars[0] / deg2rad(25);
+          throttle_value = vars[1];
+
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the 
           //   steering value back. Otherwise the values will be in between 
-          //   [-deg2rad(25), deg2rad(25] instead of [-1, 1].
+          //   [-deg2rad(25), deg2rad(25) instead of [-1, 1].
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
@@ -72,6 +112,11 @@ int main() {
            *   connected by a Green line
            */
 
+          for (unsigned int i = 2; i < vars.size(); i += 2) {
+            mpc_x_vals.push_back(vars[i]);
+            mpc_y_vals.push_back(vars[i+1]);
+          }
+
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
 
@@ -84,6 +129,11 @@ int main() {
            *   the vehicle's coordinate system the points in the simulator are 
            *   connected by a Yellow line
            */
+
+          for (unsigned int i = 0; i < xvals.size() - 1; i++) {
+            next_x_vals.push_back(xvals[i]);
+            next_y_vals.push_back(yvals[i]);
+          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
@@ -101,6 +151,10 @@ int main() {
           // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE SUBMITTING.
           std::this_thread::sleep_for(std::chrono::milliseconds(100));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+
+          auto t_end = std::chrono::system_clock::now();
+          std::chrono::duration<double> t_diff = t_end - t_start;
+          dt = t_diff.count();
         }  // end "telemetry" if
       } else {
         // Manual driving
